7f6dd848d8ee97242cc8a35b9e384149658158a9
nayarasoftware <nayarafaquir@gmail.com>
2026-03-10 14:04:20 -0400

    Primeira versão do site Mitolyn

---
+++ script.js
@@ -0,0 +1,13 @@
+const hoplink = "https://cad59jfh6igp4k17rrtb5hblex.hop.clickbank.net";
+
+document.getElementById("buyBtn1").addEventListener("click", function(){
+
+window.location.href = hoplink;
+
+});
+
+document.getElementById("buyBtn2").addEventListener("click", function(){
+
+window.location.href = hoplink;
+
+});
